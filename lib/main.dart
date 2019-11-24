dac9a668a35b8f7543a116496bd2dfc67aefb697
Saumya <singhsaumyas150@gmail.com>
2019-11-24 06:00:04 +0530

    Hello Flutter Project 💙💙�

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() //starting point of application
+{
+  runApp(
+      Center
+      (
+      child: Text(
+      "Hello Flutter!❤", textDirection: TextDirection.ltr
+      )
+      )
+        );
+}
